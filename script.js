c70a2e05392bc0efa54e53cd876e0223c173973f
tonybalde <tonybalde@hotmail.com>
2023-03-22 22:49:35 -0300

    Add new book add btn

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let myLibrary = [];
+
+function addBook() {
+
+
+}
+
+
+let myLibrary = [{book: "The Eight", author: "Katherin Neville", pages: "805", read: "read"}];
 
 function Book() {
   // the constructor...
@@ -7,4 +14,12 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+
+let newBookBtn = document.querySelector("#new-book-btn");
+newBookBtn.addEventListener("click", functio =>{
+  let newBookForm = document.querySelector("#new-book-form");
+  console.log(newBookForm);
+  newBookForm.style.display = "flex";
+})
 
